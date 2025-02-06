8ab16ec4d0eb311c55c72068e645db66eb3cb3f1
DatNguyen158 <91834256+DatNguyen158@users.noreply.github.com>
2025-02-06 16:26:44 +0700

    Add files via upload

---
+++ Giới thiệu bản thân/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Tôi là sinh viên khoa An toàn thông tin của Học viện Công nghệ Bưu chính Viễn thông", 
+    "Sở thích của tôi là đàn, hát, chơi game", 
+    "Ước mơ của tôi là trở thành 1 developer"],
+    typeSpeed: 30,
+    backSpeed: 30,
+    backDelay: 1500,
+    loop: true
+})
